92e8300e3e961e152d848bc5d8205a34222b017c
demmonic <evsubiw@gmail.com>
2018-01-14 13:12:09 -0500

    perfection

--- PUBGDL/native_utils.cpp
+++ PUBGDL/native_utils.cpp
@@ -9,6 +9,14 @@ int ubyte1(int i) {
 	return (i >> 8) & 0xFF;
 }
 
+int ubyte2(int i) {
+	return (i >> 16) & 0xFF;
+}
+
+int ubyte3(int i) {
+	return (i >> 24) & 0xFF;
+}
+
 int word4(int64_t l) {
 	return (l >> 0) & 0xFFFF;
 }
--- PUBGDL/native_utils.h
+++ PUBGDL/native_utils.h
@@ -3,4 +3,6 @@
 
 int ubyte0(int i);
 int ubyte1(int i);
+int ubyte2(int i);
+int ubyte3(int i);
 int word4(int64_t l);
